d69b7d66f62feeaf48381279dfe39de409f1b324
Pedro Rodrigues Santos Valle <54651078+pedrorsvalle@users.noreply.github.com>
2020-09-17 23:25:23 -0300

    Add files via upload

---
+++ Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author pedro
+ */
+ public class Calculadora {
+
+    private double resultado;
+
+ public void somar (double a,double b){
+     this.resultado= a + b;
+  } 
+
+ public void multiplicar (double a,double b){
+     this.resultado= a * b;
+  } 
+
+public void dividir (double a,double b){
+     this.resultado= a / b;
+  } 
+
+public void subtrair (double a,double b){
+     this.resultado= a - b;
+  } 
+
+}
+
+
+
